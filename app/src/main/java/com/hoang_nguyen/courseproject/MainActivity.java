032871f1696023603a5f5261d0c506b12c71dee5
eric <eirclee515@hotmail.com>
2017-11-30 17:25:06 -0500

    Need to figure it out how to use a taken image immediately.

--- app/src/main/java/com/hoang_nguyen/courseproject/MainActivity.java
+++ app/src/main/java/com/hoang_nguyen/courseproject/MainActivity.java
@@ -1,12 +1,20 @@
 package com.hoang_nguyen.courseproject;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
     Button resumeButton, pictureLibraryButton, takePictureButton, helpButton, quitButton;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,7 +32,39 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 finish();
                 System.exit(0);
+
+
             }
         });
+
+
+
+
+
+    }//end of create.
+
+
+
+
+
+
+    public void takePic (View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
     }
-}
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            //mImageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+
+
+
+}//end of class.
