f7c2f7faf79e9e3f25c4803ac362b3c7a328a48b
eric <eirclee515@hotmail.com>
2017-12-08 14:11:20 -0500

    In playPuzzle, now we need to figure it out - how to use iamge for puzzle. - how to use selected image from album

--- app/src/main/java/com/hoang_nguyen/courseproject/MainActivity.java
+++ app/src/main/java/com/hoang_nguyen/courseproject/MainActivity.java
@@ -1,45 +1,31 @@
 package com.hoang_nguyen.courseproject;
-
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-
-
 public class MainActivity extends AppCompatActivity {
 
-    static final int REQUEST_IMAGE_CAPTURE = 1;
-    static final int REQUEST_TAKE_ALBUM = 2;
-    static final int REQUEST_IMAGE_CROP = 3;
-    static final int REQUEST_TAKE_PHOTO = 4;
-
     Intent intent;
-    Uri photoURI, albumURI; // URI for photo and album.
-
-
+    boolean isAlbum = false;
+    boolean isCamera = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
     }//end of create.
 
-    public void resumeGame (View view){}//end of resume game button
+    public void resumeGame (View view){
+    }//end of resume game button
 
     public void lordPicture(View view){
-    //Open a photo album in the device and user can see pictures.
-    //create new intent to access the photo album
 
-        intent = new Intent(Intent.ACTION_PICK);
-        intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
-        startActivityForResult(intent, REQUEST_TAKE_ALBUM);
-
+        isAlbum = true;
+        intent = new Intent(this, playPuzzle.class);
+        intent.putExtra("checkAlbum", isAlbum);
+        startActivity(intent);
     }//end of lord picture method.
 
     public void help (View view){
@@ -56,46 +42,12 @@ public class MainActivity extends AppCompatActivity {
 
     public void takePic (View view) {
     //take a picture fucntion.
-
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-        if(takePictureIntent.resolveActivity(getPackageManager()) != null){
-
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-
-        }//end of if statement.
+        isCamera = true;
+        intent = new Intent(this, playPuzzle.class);
+        intent.putExtra("checkPic", isCamera);
+        startActivity(intent);
 
     }//end of takePic
 
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-        switch (requestCode){
-
-
-            case REQUEST_IMAGE_CAPTURE:
-                //after taking picture, grab a taken photo URI
-                //then start playPuzzle class.
-
-                Bitmap photo = (Bitmap) data.getExtras().get("Data");
-                intent = new Intent(this, playPuzzle.class);
-                intent.putExtra("image", photo);
-                startActivity(intent);
-
-                break;
-
-
-            case REQUEST_TAKE_ALBUM:
-                //lord an image from the album.
-
-                break;
-            case REQUEST_TAKE_PHOTO:
-
-                break;
-
-        }
-
-    }//end of onActivityResult.
 
 }//end of class.
--- app/src/main/java/com/hoang_nguyen/courseproject/playPuzzle.java
+++ app/src/main/java/com/hoang_nguyen/courseproject/playPuzzle.java
@@ -1,12 +1,17 @@
 package com.hoang_nguyen.courseproject;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 
 public class playPuzzle extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_TAKE_ALBUM = 2;
+    static final int REQUEST_IMAGE_CROP = 3;
+    static final int REQUEST_TAKE_PHOTO = 4;
 
     Intent intent;
     ImageView imageView;
@@ -16,14 +21,78 @@ public class playPuzzle extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_puzzle);
-
         this.setTitle("Puzzle Time");
 
-        Bitmap bitmap = (Bitmap) intent.getParcelableExtra("image");
-        imageView.setImageBitmap(bitmap);
+        imageView = (ImageView) findViewById(R.id.setImage);
+
+        boolean isAlbum = getIntent().getExtras().getBoolean("checkAlbum");
+        boolean isCamera = getIntent().getExtras().getBoolean("checkPic");
+
+        if (isAlbum) {
+        //if user pick album, then load images.
+            album();
+        }
+
+        if (isCamera) {
+            //if user select taking picture, then run camera.
+           // camera();
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }//end of takePic
+
+        }
 
 
     }//end of on Create
 
+    public void camera() {
+        //take a picture fucntion.
+
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }//end of takePic
+
+    }
+    public void album (){
+        //Open a photo album in the device and user can see pictures.
+        //create new intent to access the photo album
+        intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+        startActivityForResult(intent, REQUEST_TAKE_ALBUM);
+
+    }//end of lord picture method.
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        imageView = (ImageView) findViewById(R.id.setImage);
+
+        switch (requestCode){
+            case REQUEST_IMAGE_CAPTURE:
+                //after taking picture, grab a taken photo URI
+                //then start playPuzzle class.
+//                Bitmap photo = (Bitmap) data.getExtras().get("Data");
+//                intent = new Intent(this, playPuzzle.class);
+//                intent.putExtra("image", photo);
+//                startActivity(intent);
+
+                imageView.setImageURI(data.getData());
+
+                break;
+
+
+            case REQUEST_TAKE_ALBUM:
+                //lord an image from the album.
+                break;
+            case REQUEST_TAKE_PHOTO:
+                break;
+
+        }
+
+    }//end of onActivityResult.
 
 }
