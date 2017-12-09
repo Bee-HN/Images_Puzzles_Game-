813d7ceaf8aa3fec66ef1e4a23dfed03711c0610
eric <eirclee515@hotmail.com>
2017-12-08 19:16:36 -0500

    hi

--- app/src/main/java/com/hoang_nguyen/courseproject/playPuzzle.java
+++ app/src/main/java/com/hoang_nguyen/courseproject/playPuzzle.java
@@ -11,7 +11,6 @@ import android.provider.MediaStore;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 
 import java.io.File;
@@ -22,8 +21,6 @@ public class playPuzzle extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
     static final int REQUEST_TAKE_ALBUM = 2;
-
-    public static Button quitbtn;
 
     Intent intent;
     ImageView imageView;
